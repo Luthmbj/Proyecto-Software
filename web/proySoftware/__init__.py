39513727909d91b90239ba1589158718d2ec322b
DarFig <log.Dariel@gmail.com>
2017-03-01 17:49:14 -0500

    modelos

--- web/proySoftware/__init__.py
+++ web/proySoftware/__init__.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://proySoftware:software@localhost/software"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bbdd = SQLAlchemy(app)
 
 #@app.route('/')
